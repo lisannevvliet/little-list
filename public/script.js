46fbfd0a22030fd7ad8ab76479e2f0a856868bc9
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-22 10:07:14 +0200

    Clone ju5tu5/barebonechat

--- public/script.js
+++ public/script.js
@@ -1,68 +1,16 @@
 let socket = io()
-let itemList = document.querySelector('form fieldset')
-let textInput = document.querySelector('input[type="text"]')
+let messages = document.querySelector('section ul')
+let input = document.querySelector('input')
 
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault()
-  if (textInput.value) {
-    socket.emit('item', textInput.value)
-    textInput.value = ''
+  if (input.value) {
+    socket.emit('message', input.value)
+    input.value = ''
   }
 })
 
-socket.on('item', item => {
-  let html = document.createElement('li')
-  html.appendChild(Object.assign(document.createElement('input'), { 
-    id: item,
-    type: "checkbox"
-  }))
-  html.appendChild(Object.assign(document.createElement('label'), { 
-    textContent: item,
-    htmlFor: item
-  }))
-
-  itemList.appendChild(html)
-
-  itemList.scrollTop = itemList.scrollHeight
-
-  // Add event listener.
-  let createdItem = document.querySelector(`#${item}`)
-  createdItem.addEventListener("change", _event => {
-    if (createdItem.checked) {
-      socket.emit("checked", createdItem.id)
-    } else {
-      socket.emit("unchecked", createdItem.id)
-    }
-  })
+socket.on('message', message => {
+  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
+  messages.scrollTop = messages.scrollHeight
 })
-
-socket.on("checked", item => {
-  console.log(item + " is checked (client-side).")
-  let changedItem = document.querySelector(`#${item}`)
-
-  if (!changedItem.checked) {
-    changedItem.checked = true;
-  }
-})
-
-socket.on("unchecked", item => {
-  console.log(item + " is unchecked (client-side).")
-  let changedItem = document.querySelector(`#${item}`)
-
-  if (changedItem.checked) {
-    changedItem.checked = false;
-  }
-})
-
-textInput.addEventListener("keypress", function() {
-  setTimeout(function() { socket.emit("doneTyping") }, 3000)
-  socket.emit("typing")
-})
-
-socket.on("typing", () =>
-  document.querySelector("#typing").textContent = "Someone is typing..."
-)
-
-socket.on("doneTyping", () =>
-  document.querySelector("#typing").textContent = ""
-)
--- server.js
+++ server.js
@@ -6,38 +6,19 @@ const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
-require('dotenv').config()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 4242
 
 app.use(express.static(path.resolve('public')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
 
-  socket.on('item', (item) => {
-    io.emit('item', item)
+  socket.on('message', (message) => {
+    io.emit('message', message)
   })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
-  })
-
-  socket.on("checked", (item) => {
-    console.log(item + " is checked (server-side).")
-    io.emit("checked", item)
-  })
-
-  socket.on("unchecked", (item) => {
-    console.log(item + " is unchecked (server-side).")
-    io.emit("unchecked", item)
-  })
-
-  socket.on("typing", () => {
-    io.emit("typing")
-  })
-
-  socket.on("doneTyping", () => {
-    io.emit("doneTyping")
   })
 })
 
