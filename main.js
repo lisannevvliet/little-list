d3d7347f42d4a66e1e80af9332b39f16c8b5128f
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-22 10:52:19 +0200

    Implement Handlebars and personalize main.js

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-/*
-https://socket.io/get-started/chat
-*/
-const express = require('express')
+// Import Dotenv.
+require("dotenv").config()
+// Import Express.
+const express = require("express")
+// Initialise Express.
 const app = express()
-const http = require('http').createServer(app)
-const path = require('path')
-const io = require('socket.io')(http)
-const port = process.env.PORT || 4242
+// Create an HTTP server.
+const server = require("http").createServer(app)
+// Import and initialise Socket.IO.
+const io = require("socket.io")(server)
+// Import Handlebars.
+const handlebars = require("express-handlebars")
 
-app.use(express.static(path.resolve('static')))
+// Render static files.
+app.use(express.static("static"))
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+// Set the view engine to Handlebars.
+app.engine("handlebars", handlebars.engine())
+app.set("view engine", "handlebars")
 
-  socket.on('message', (message) => {
-    io.emit('message', message)
+io.on("connection", (socket) => {
+  console.log("A user connected.")
+
+  socket.on("message", (message) => {
+    io.emit("message", message)
   })
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on("disconnect", () => {
+    console.log("User disconnected.")
   })
 })
 
-http.listen(port, () => {
-  console.log('listening on port ', port)
+// Set and log the port for the HTTP server.
+server.listen(process.env.PORT, () => {
+  console.log(`HTTP server running at http://localhost:${process.env.PORT}.`)
+})
+
+// Listen to all GET requests on /.
+app.get("/", (_req, res) => {
+  // Load the index page.
+  res.render("index")
 })
