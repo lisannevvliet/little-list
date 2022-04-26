3736b35d08058d2713807b2ad0fc38467b8aae49
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-26 10:59:38 +0200

    Change indentation

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.set("view engine", "handlebars")
 
 // Parse incoming requests.
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }))
 
 let connected = []
@@ -32,109 +32,109 @@ let trivia
 let answers = []
 
 io.on("connection", (socket) => {
-  // Add the connection ID to the list of connected clients.
-  connected.push(socket.id)
-
-  // Emit the amount of connected clients.
-  io.emit("connection", connected.length)
-
-  if (trivia == undefined) {
-    // Get the trivia from the API.
-    fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple")
-    .then((response) => {
-      response.json()
-    })
-    .then((data) => {
-      // Decode the trivia's question.
-      data.results[0].question = entities.decodeHTML(data.results[0].question)
-
-      // Decode the trivia's incorrect answers.
-      data.results[0].incorrect_answers.forEach((element, index) => {
-        data.results[0].incorrect_answers[index] = entities.decodeHTML(element)
-      })
-
-      // Add an array of randomized answers and decode the trivia's correct answer.
-      data.results[0].answers = [entities.decodeHTML(data.results[0].correct_answer)].concat(data.results[0].incorrect_answers).sort(function() {
-        return 0.5 - Math.random()
-      })
-
-      // Emit the trivia.
-      io.emit("trivia", data.results[0])
-
-      // Save the most recent trivia.
-      trivia = data.results[0]
-
-      // Clear the answers.
-      answers = []
-    })
-  } else {
-    // Emit the existing trivia.
-    io.emit("trivia", trivia)
-  }
-
-  socket.on("disconnect", () => {
-    // Remove the connection ID from the list of connected clients.
-    connected = connected.filter(function(id) {
-      return id != socket.id
-    })
+    // Add the connection ID to the list of connected clients.
+    connected.push(socket.id)
 
     // Emit the amount of connected clients.
     io.emit("connection", connected.length)
-  })
 
-  socket.on("answer", (answer) => {
-    io.emit("answer", answer)
-
-    answers.push(answer)
-
-    if (answers.length == connected.length) {
-      // Get the trivia from the API.
-      fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple")
+    if (trivia == undefined) {
+        // Get the trivia from the API.
+        fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple")
         .then((response) => {
-          response.json()
+            response.json()
         })
         .then((data) => {
-          // Decode the trivia's question.
-          data.results[0].question = entities.decodeHTML(data.results[0].question)
+            // Decode the trivia's question.
+            data.results[0].question = entities.decodeHTML(data.results[0].question)
 
-          // Decode the trivia's incorrect answers.
-          data.results[0].incorrect_answers.forEach((element, index) => {
-            data.results[0].incorrect_answers[index] = entities.decodeHTML(element)
-          })
+            // Decode the trivia's incorrect answers.
+            data.results[0].incorrect_answers.forEach((element, index) => {
+                data.results[0].incorrect_answers[index] = entities.decodeHTML(element)
+            })
 
-          // Add an array of randomized answers and decode the trivia's correct answer.
-          data.results[0].answers = [entities.decodeHTML(data.results[0].correct_answer)].concat(data.results[0].incorrect_answers).sort(function() {
-            return 0.5 - Math.random()
-          })
+            // Add an array of randomized answers and decode the trivia's correct answer.
+            data.results[0].answers = [entities.decodeHTML(data.results[0].correct_answer)].concat(data.results[0].incorrect_answers).sort(function() {
+                return 0.5 - Math.random()
+            })
 
-          io.emit("trivia", data.results[0])
+            // Emit the trivia.
+            io.emit("trivia", data.results[0])
 
-          // Save the most recent trivia.
-          trivia = data.results[0]
+            // Save the most recent trivia.
+            trivia = data.results[0]
 
-          // Clear the answers.
-          answers = []
+            // Clear the answers.
+            answers = []
         })
+    } else {
+        // Emit the existing trivia.
+        io.emit("trivia", trivia)
     }
-  })
+
+    socket.on("disconnect", () => {
+        // Remove the connection ID from the list of connected clients.
+        connected = connected.filter(function(id) {
+            return id != socket.id
+        })
+
+        // Emit the amount of connected clients.
+        io.emit("connection", connected.length)
+    })
+
+    socket.on("answer", (answer) => {
+        io.emit("answer", answer)
+
+        answers.push(answer)
+
+        if (answers.length == connected.length) {
+            // Get the trivia from the API.
+            fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple")
+                .then((response) => {
+                    response.json()
+                })
+                .then((data) => {
+                    // Decode the trivia's question.
+                    data.results[0].question = entities.decodeHTML(data.results[0].question)
+
+                    // Decode the trivia's incorrect answers.
+                    data.results[0].incorrect_answers.forEach((element, index) => {
+                        data.results[0].incorrect_answers[index] = entities.decodeHTML(element)
+                    })
+
+                    // Add an array of randomized answers and decode the trivia's correct answer.
+                    data.results[0].answers = [entities.decodeHTML(data.results[0].correct_answer)].concat(data.results[0].incorrect_answers).sort(function() {
+                        return 0.5 - Math.random()
+                    })
+
+                    io.emit("trivia", data.results[0])
+
+                    // Save the most recent trivia.
+                    trivia = data.results[0]
+
+                    // Clear the answers.
+                    answers = []
+                })
+        }
+    })
 })
 
 // Set and log the port for the HTTP server.
 server.listen(process.env.PORT, () => {
-  console.log(`HTTP server running at http://localhost:${process.env.PORT}.`)
+    console.log(`HTTP server running at http://localhost:${process.env.PORT}.`)
 })
 
 // Listen to all GET requests on /.
 app.get("/", (_req, res) => {
-   // Load the index page.
-   res.render("index")
+     // Load the index page.
+     res.render("index")
 })
 
 // Listen to all POST requests on /.
 app.post("/", (req, res) => {
-  // Load the trivia page with the name and whether it ends with an "s".
-  res.render("trivia", {
-    name: req.body.name,
-    s: !(req.body.name.endsWith("s") || req.body.name.endsWith("S"))
-  })
+    // Load the trivia page with the name and whether it ends with an "s".
+    res.render("trivia", {
+        name: req.body.name,
+        s: !(req.body.name.endsWith("s") || req.body.name.endsWith("S"))
+    })
 })
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,67 +1,67 @@
 function $(element) {
-  // Return the Element object of the corresponding element.
-  return document.querySelector(element)
+    // Return the Element object of the corresponding element.
+    return document.querySelector(element)
 }
 
 // Check if the current page is the trivia.
 if ($("#trivia")) {
-  // Initialise Socket.IO.
-  let socket = io()
+    // Initialise Socket.IO.
+    let socket = io()
 
-  let correct
+    let correct
 
-  // Listen to clicks on the answer buttons.
-  document.querySelectorAll("#trivia #answers button").forEach((item) => {
-    item.addEventListener("click", (event) => {
-      if (event.target.innerText == correct) {
-        // Make the selected button green.
-        event.target.classList.add("green")
-      } else {
-        // Make the selected button red.
-        event.target.classList.add("red")
+    // Listen to clicks on the answer buttons.
+    document.querySelectorAll("#trivia #answers button").forEach((item) => {
+        item.addEventListener("click", (event) => {
+            if (event.target.innerText == correct) {
+                // Make the selected button green.
+                event.target.classList.add("green")
+            } else {
+                // Make the selected button red.
+                event.target.classList.add("red")
 
-        document.querySelectorAll("#trivia #answers button").forEach((element) => {
-          if (element.innerText == correct) {
-            // Make the correct button green.
-            element.classList.add("green")
-          }
+                document.querySelectorAll("#trivia #answers button").forEach((element) => {
+                    if (element.innerText == correct) {
+                        // Make the correct button green.
+                        element.classList.add("green")
+                    }
+                })
+            }
+
+            // Disable all buttons.
+            document.querySelectorAll("#trivia #answers button").forEach((item) =>
+                item.disabled = true
+            )
+
+            // Send the answer to the socket after half a second.
+            setTimeout(function() {
+                socket.emit("answer", event.target.innerText)
+            }, 500)
         })
-      }
-
-      // Disable all buttons.
-      document.querySelectorAll("#trivia #answers button").forEach((item) =>
-        item.disabled = true
-      )
-
-      // Send the answer to the socket after half a second.
-      setTimeout(function() {
-        socket.emit("answer", event.target.innerText)
-      }, 500)
     })
-  })
 
-  socket.on("connection", (length) => {
-    // Update the amount of players.
-    $("#trivia #connected").innerHTML = `<span></span>${length} players`
-  })
-
-  socket.on("trivia", (trivia) => {
-    // Update the trivia's question.
-    $("#trivia #question").innerText = trivia.question
-
-    // Update the trivia's correct answer.
-    correct = trivia.correct_answer
-
-    document.querySelectorAll("#trivia #answers button").forEach((item, index) => {
-      // Remove the result of the previously selected answer.
-      item.classList.remove("green")
-      item.classList.remove("red")
-
-      // Enable all buttons.
-      item.disabled = false
-
-      // Update the trivia's answers.
-      item.innerText = trivia.answers[index]
+    socket.on("connection", (length) => {
+        // Update the amount of players.
+        $("#trivia #connected").innerHTML = `<span></span>${length} players`
     })
-  })
+
+    socket.on("trivia", (trivia) => {
+        // Update the trivia's question.
+        $("#trivia #question").innerText = trivia.question
+
+        // Update the trivia's correct answer.
+        correct = trivia.correct_answer
+
+        document.querySelectorAll("#trivia #answers button").forEach((item, index) => {
+            // Remove the result of the previously selected answer.
+            item.classList.remove("green")
+            item.classList.remove("red")
+
+            // Enable all buttons.
+            item.disabled = false
+
+            // Update the trivia's answers.
+            item.innerText = trivia.answers[index]
+        })
+    })
 }
